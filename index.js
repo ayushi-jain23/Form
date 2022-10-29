ab3bf88c7bf98d8910743fdc80a5a133f0d2761b
Ayushi Jain <ashi02jainaj@gmail.com>
2022-10-29 21:24:38 +0530

    commit

--- index.js
+++ index.js
@@ -14,69 +14,58 @@ form.addEventListener('focusin', function (e) {
 form.addEventListener('focusout', function (e) {
     let parentsElement = e.target.parentElement;
     let child = parentsElement.querySelector('input');
-    // console.log(child);
 
     let error = document.createElement('div');
     error.classList.add('error');
-    error.innerHTML = 'This field is mandatory.';
 
-    if (child.value === '') {
+    if (child && child.value === '') {
+        error.innerHTML = 'This field is mandatory.';
         parentsElement.append(error);
     } else {
         let classOfClickedInput = child.getAttribute('class');
         console.log(child.getAttribute('class'));
 
-        //mobile number validation
-        if (classOfClickedInput === 'phnum') {
-            if (child.value.length > 0 && child.value.length <= 9) {
-                let error = document.createElement('div');
-                error.classList.add('error');
-                error.innerHTML = 'please enter a valid ph num';
-                child.parentElement.append(error);
-            }
-        }
-
         //password validation
-        else if (classOfClickedInput === 'password') {
-            let passRegex =
-                /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+        if (classOfClickedInput === 'input_password') {
+            let passRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
             if (child.value.match(passRegex) || child.value === '') {
                 return true;
             } else {
-                let error = document.createElement('div');
-                error.classList.add('error');
-                error.innerHTML =
-                    'pass must have 8-15 characters, an uppercase letter, a lowercase letter, a number and a special character';
+                error.innerHTML = 'Pass must have 8-15 characters, an uppercase letter, a lowercase letter, a number and a special character';
                 child.parentElement.append(error);
             }
         }
 
         //confirm pass validation
-        else if (classOfClickedInput === 'repassword') {
-            let password = document.querySelector('.password');
+        else if (classOfClickedInput === 'input_repassword') {
+            let password = document.querySelector('.input_password');
             if (password.value !== child.value && child.value !== '') {
-                /////replace password
-                let error = document.createElement('div');
-                error.classList.add('error');
-                error.innerHTML = "pass doesn't match";
+                error.innerHTML = "Pass doesn't match";
+                child.parentElement.append(error);
+            }
+        }
+
+        //phone number validation
+        else if (classOfClickedInput === 'input_phnum') {
+            if (child.value.length > 0 && child.value.length <= 9) {
+                error.innerHTML = 'Please enter a valid ph num';
                 child.parentElement.append(error);
             }
         }
 
         //current email validation
-        else if (classOfClickedInput === 'email') {
-            let mailcheck = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-            if (child.value.match(mailcheck) || child.value === '') {
+        else if (classOfClickedInput === 'input_email') {
+            let mailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (child.value.match(mailRegex) || child.value === '') {
                 return true;
             } else {
-                let error = document.createElement('div');
-                error.classList.add('error');
-                error.innerHTML =
-                    'enter valid email in the format anystring@anystring.any';
+                error.innerHTML = 'Enter valid email in the format anystring@anystring.any';
                 child.parentElement.append(error);
             }
         }
     }
 });
 
-// submit.addEventListener('click', function (e) {});
+submit.addEventListener('click', function (e) {
+    alert("Form submitted successfully")
+});
